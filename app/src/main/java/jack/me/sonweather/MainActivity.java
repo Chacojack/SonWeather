86262a89e31410f85f4f804003a15989550e4d88
Jack Chai <zejian.chai@gmail.com>
2016-10-31 20:43:48 +0800

    加入需要用到的核心框架

--- app/src/main/java/jack/me/sonweather/MainActivity.java
+++ app/src/main/java/jack/me/sonweather/MainActivity.java
@@ -1,18 +1,29 @@
 package jack.me.sonweather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @BindView(R.id.button)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        ButterKnife.bind(this);
 
+        button.setOnClickListener(v -> Log.d(TAG, "onClick: "));
+
+    }
 
 
 }
